6c3134a8c473027d7973efca02381264b022e0ec
JuanRodriguez10 <juan.rodriguez10@gmail.com>
2024-11-12 11:35:25 -0500

    Ingreso de Spring Boot

---
+++ src/main/java/com/proyecto/challenge_literatura_alura/ChallengeLiteraturaAluraApplication.java
@@ -0,0 +1,13 @@
+package com.proyecto.challenge_literatura_alura;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChallengeLiteraturaAluraApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ChallengeLiteraturaAluraApplication.class, args);
+	}
+
+}
